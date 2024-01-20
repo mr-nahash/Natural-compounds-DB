02a202d8bb2215a422c7a17f82e671a3ca36bda2
Fernando Martinez Urrutia <89673693+mr-nahash@users.noreply.github.com>
2024-01-19 18:37:20 -0600

    last successful deployment of 1.0.1

--- db_webapp/next.config.js
+++ db_webapp/next.config.js
@@ -1,6 +1,8 @@
 const CopyPlugin = require("copy-webpack-plugin");
-
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig)
