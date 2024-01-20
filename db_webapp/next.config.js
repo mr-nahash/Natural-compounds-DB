5243c0b70078b466cfa168a471ca5f1dcf2e78ab
Fernando Martinez Urrutia <89673693+mr-nahash@users.noreply.github.com>
2024-01-20 16:54:01 -0600

    bundle analyzer

--- db_webapp/next.config.js
+++ db_webapp/next.config.js
@@ -1,8 +1,13 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+});
 
-module.exports = withBundleAnalyzer(nextConfig)
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@heroicons/react/24', '@material-tailwind/react'],
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
