555ffc55a325b6ebc4f140dd2bc450b14c358d87
root <root@ubuntu-cloudpanel.localhost>
2024-02-12 09:36:02 +0000

    trivial changes to main folder

--- db_webapp/components/MoleculeEdit.js
+++ db_webapp/components/MoleculeEdit.js
@@ -1,11 +1,7 @@
-// Import the necessary dependencies
 import React, { useEffect, useRef, useState } from 'react';
 import { Button } from '@material-tailwind/react';
 import {
-  CloudArrowUpIcon,
   ArrowLongRightIcon,
-  ArrowPathIcon,
-  BookmarkIcon,
 } from '@heroicons/react/24/outline';
 import axios from 'axios'; // Import Axios for making HTTP requests
 import MoleculeGallery from './MolGallery';
@@ -19,21 +15,24 @@ export default function SearchByTanimoto() {
   const [isLoading, setIsLoading] = useState(false); // Add loading state
 
   useEffect(() => {
+    let editor = editorRef.current;
 
-    const script = document.createElement('script');
-    script.src = 'https://molsoft.com/lib/moledit.js';
-    script.type = 'text/javascript';
-    script.async = true;
-    script.onload = () => {
-      const editor = new ChemicalView('c1ccccc1', 'myEditor');
-      document.moledit = editor;
-      editorRef.current = editor;
-    };
-    document.body.appendChild(script);
+    if (!editor) {
+      const script = document.createElement('script');
+      script.src = 'https://molsoft.com/lib/moledit.js';
+      script.type = 'text/javascript';
+      script.async = true;
+      script.onload = () => {
+        editor = new ChemicalView('c1ccccc1', 'myEditor');
+        document.moledit = editor;
+        editorRef.current = editor;
+      };
+      document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
   }, []);
 
   const calculateTanimoto = async (smiles) => {
@@ -42,7 +41,7 @@ export default function SearchByTanimoto() {
       const response = await axios.post('/api/calculate_tanimoto', { smiles });
       const tanimotoWithMolecules = response.data;
       setTanimotoWithMolecules(tanimotoWithMolecules);
-      console.log('Tanimoto data:',tanimotoWithMolecules);
+      console.log('Tanimoto data:', tanimotoWithMolecules);
     } catch (error) {
       setError(error.message);
     } finally {
@@ -60,33 +59,31 @@ export default function SearchByTanimoto() {
 
   return (
     <div className="grid grid-cols-3 gap-4 w-full h-full p-10">
-    {/* Left Column */}
-    <div className="col-span-1" style={{ height: '500px' }}>
-      <div id="myEditor" style={{ width: '100%', height: '100%', border: '2px solid gray' }}>
+      {/* Left Column */}
+      <div className="col-span-1" style={{ height: '500px' }}>
+        <div id="myEditor" style={{ width: '100%', height: '100%', border: '2px solid gray' }}></div>
+        <div>
+          <Button
+            variant="text"
+            className="flex items-center gap-2 relative"
+            onClick={handleGetSmiles}
+          >
+            Search <ArrowLongRightIcon strokeWidth={2} className="h-5 w-5" />
+          </Button>
+        </div>
       </div>
-      <div>
-        <Button
-          variant="text"
-          className="flex items-center gap-2 relative"
-          onClick={handleGetSmiles}
-        >
-          Search <ArrowLongRightIcon strokeWidth={2} className="h-5 w-5" />
-        </Button>
+      {/* Right Column */}
+      <div className="col-span-2">
+        <div>
+          {isLoading ? (
+            // Render a loading indicator here if data is loading
+            <p>Loading...</p>
+          ) : tanimotoWithMolecules ? (
+            // Conditionally render the MoleculeGallery component when data is available
+            <MoleculeGallery data={tanimotoWithMolecules} itemsPerPage={itemsPerPage} />
+          ) : null}
+        </div>
       </div>
     </div>
-    {/* Right Column */}
-    <div className="col-span-2">
-    <div>
-        {isLoading ? (
-          // Render a loading indicator here if data is loading
-          <p>Loading...</p>
-        ) : tanimotoWithMolecules ? (
-          // Conditionally render the MoleculeGallery component when data is available
-          <MoleculeGallery data={tanimotoWithMolecules} itemsPerPage={itemsPerPage}/>
-        ) : null}
-      </div>
-    </div>
-  </div>
   );
 }
-
